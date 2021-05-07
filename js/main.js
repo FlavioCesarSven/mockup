def9d9bba6b3e1e4aa0424cc83adf7ee6dbcee49
Flavio <flavio.svenps@gmail.com>
2021-05-07 14:59:09 -0500

    falta el search implementarlo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 (function ($) {
     "use strict";
+
+    
     
     // Back to top button
     $(window).scroll(function () {
@@ -45,73 +47,101 @@
     
     // Main carousel
     $(".carousel .owl-carousel").owlCarousel({
-        autoplay: true,
+        autoplay: false,
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
         items: 1,
-        smartSpeed: 300,
+        // smartSpeed: 300,
         dots: false,
         loop: true,
         nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ]
+        // navText : [
+        //     '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+        //     '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        // ]
     });
     
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
-        });
-        console.log($videoSrc);
+    // // Modal Video
+    // $(document).ready(function () {
+    //     var $videoSrc;
+    //     $('.btn-play').click(function () {
+    //         $videoSrc = $(this).data("src");
+    //     });
+    //     console.log($videoSrc);
 
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
+    //     $('#videoModal').on('shown.bs.modal', function (e) {
+    //         $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+    //     })
 
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
-    });
+    //     $('#videoModal').on('hide.bs.modal', function (e) {
+    //         $("#video").attr('src', $videoSrc);
+    //     })
+    // });
     
-    // jQuery counterUp
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
+    // // jQuery counterUp
+    // $('[data-toggle="counter-up"]').counterUp({
+    //     delay: 10,
+    //     time: 2000
+    // });
 
 
     // Testimonials carousel
-    $(".testimonials-carousel").owlCarousel({
+    // $(".testimonials-carousel").owlCarousel({
+    //     autoplay: true,
+    //     animateIn: 'slideInDown',
+    //     animateOut: 'slideOutDown',
+    //     items: 1,
+    //     smartSpeed: 450,
+    //     dots: false,
+    //     loop: true,
+    //     nav : true,
+    //     navText : [
+    //         '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+    //         '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+    //     ],
+    //     responsive: {
+    //         0:{
+    //             items:1
+    //         },
+    //         576:{
+    //             items:1
+    //         },
+    //         768:{
+    //             items:2
+    //         },
+    //         992:{
+    //             items:3
+    //         }
+    //     }
+    // });
+
+
+    //SLIDER
+ 
+       $('.logo-area').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
         autoplay: true,
-        animateIn: 'slideInDown',
-        animateOut: 'slideOutDown',
-        items: 1,
-        smartSpeed: 450,
-        dots: false,
-        loop: true,
-        nav : true,
-        navText : [
-            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
-            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
-        ],
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
+        autoplaySpeed: 700,
+        // arrows: false,
+        prevArrow:'<i class="fas fa-angle-left   left_arrow"></i>',
+        nextArrow:'<i class="fas fa-angle-right   right_arrow"></i>',
+        pauseOnHover: false,
+        responsive:[{
+            breakpoint: 768,
+            settings:{
+                slidesToShow: 4,
+                arrows: false,
             }
-        }
-    });
+        },
+       {
+           breakpoint: 520,
+           settings:{
+               slidesToShow: 3,
+               arrows: false,
+           }
+       }]});
+
     
     
     // Blogs carousel
